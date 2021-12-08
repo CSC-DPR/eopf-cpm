d123cad95235c6016e0a33eaa2aba06de315652f
Clément Dubos <clement.dubos.ext@csgroup.eu>
2021-12-08 18:32:21 +0100

    feat: add exceptions module and improve core api

--- eopf/product/core.py
+++ eopf/product/core.py
@@ -8,9 +8,45 @@ Climate and Forecast conventions: https://cfconventions.org/
 
 from abc import ABC, abstractmethod
 from collections.abc import MutableMapping
-from typing import Any, Callable, Hashable, Iterator, List, Mapping, Optional, Union
+from typing import Any, Iterable, Iterator, KeysView, List, Optional, Union
 
 from xarray import DataArray
+
+from eopf import exceptions
+
+
+def search(eoobject: Union["EOGroup", "EOProduct"], path, already_done: List["EOGroup"] = None):
+    """
+    Climate and forecast convention search mechanisme for group
+    https://cfconventions.org/Data/cf-conventions/cf-conventions-1.9/cf-conventions.html#_scope
+    """
+    already_done = already_done or []
+    already_done.append(eoobject)
+
+    root, _, subpaths = path.partition("/")
+    if not root:  # absolute path
+        if not isinstance(eoobject, EOProduct):
+            parent = eoobject.parent
+            while parent.parent is not None:
+                parent = parent.parent
+        else:
+            parent = eoobject
+        return parent.search(subpaths)
+    elif root and subpaths:  # relative path
+        current_item = eoobject.groups.get(root)
+        if current_item:
+            return current_item.search(subpaths)
+        return current_item
+    else:  # proximity
+        current_item = eoobject.get(root)
+        if current_item:
+            return current_item
+        for group in eoobject.parent.groups.values():
+            if group not in already_done:
+                find = group.search(root, already_done=already_done)
+                if find:
+                    return find
+    return
 
 
 class EOProperties(ABC):
@@ -47,13 +83,6 @@ class EOProperties(ABC):
         Compliant to the Common data model.
         """
 
-    @parent.setter
-    @abstractmethod
-    def parent(self, parent: Union["EOProduct", "EOGroup"]) -> None:
-        """
-        Set the direct parent of an EO Object.
-        """
-
 
 class EOVariable(EOProperties):
     """Earth Observation Variable definition
@@ -66,125 +95,174 @@ class EOVariable(EOProperties):
     - https://docs.dask.org/en/stable/custom-collections.html
     """
 
-    _ndarray: DataArray
+    __slots__ = (
+        "_ndarray",
+        "_parent",
+    )
 
-    @abstractmethod
-    def __array_ufunc__(self, ufunc, method, *inputs, **kwargs):
-        """Must be compliant to the NEP 13.
-        https://numpy.org/neps/nep-0013-ufunc-overrides.html
-        """
+    def __types__(self) -> None:
+        self._ndarray: DataArray
+        self._parent: Optional["EOGroup"] = None
 
-    @abstractmethod
-    def __array_function__(self, func, types, args, kwargs):
-        """Must be compliant to th NEP 18.
-        https://numpy.org/neps/nep-0018-array-function-protocol.html
-        """
+    def __init__(self, ndarray: DataArray):
+        self.__types__()
+        self._ndarray = ndarray
 
-    @abstractmethod
-    def __dask_graph__(self) -> Union[Mapping, None]:
-        """ """
+    def __str__(self):
+        return self.__repr__()
 
-    @abstractmethod
-    def __dask_keys__(self) -> list:
-        """ """
-
-    @abstractmethod
-    def __dask_layers__(self) -> tuple:
-        """ """
-
-    @abstractmethod
-    def __dask_postcompute__(self) -> tuple[Callable, tuple]:
-        """ """
-
-    @abstractmethod
-    def __dask_postpersist__(self) -> tuple[Callable, tuple]:
-        """ """
-
-    @abstractmethod
-    def __dask_tokenize__(self) -> Hashable:
-        """Dask deterministic Hashing:
-        https://docs.dask.org/en/stable/custom-collections.html#deterministic-hashing"""
-
-    @staticmethod
-    @abstractmethod
-    def __dask_optimize__(dsk, keys, **kwargs) -> MutableMapping:
-        """ """
-
-    @staticmethod
-    @abstractmethod
-    def __dask_scheduler__(dsk, keys, **kwargs):
-        """ """
+    def __repr__(self) -> str:
+        parent_part = f"(parent={self.parent.name})" if self._parent else ""
+        return f"[EOGVariable]{parent_part}(dims={self.dims}) {self.name}"
 
     @property
-    @abstractmethod
-    def chunks(self) -> Optional[tuple[tuple[int, ...], ...]]:
-        """Accessor to the chunks"""
+    def attrs(self) -> MutableMapping[str, Any]:
+        return self._ndarray.attrs
 
-    @abstractmethod
-    def rechunk(
-        self,
-        chunks_shape: Union[
-            Mapping[Any, Union[None, int, tuple[int, ...]]],
-            int,
-            tuple[int, ...],
-            tuple[tuple[int, ...], ...],
-        ],
-    ) -> None:
-        """Change chunk shape / size"""
+    @property
+    def dims(self) -> tuple[str]:
+        return self._ndarray.dims
 
-    @abstractmethod
-    def map_chunk(self, func):
-        """map function on each chunk"""
+    @property
+    def name(self) -> Optional[str]:
+        return self._ndarray.name
+
+    @property
+    def parent(self) -> "EOGroup":
+        if self._parent is None:
+            raise exceptions.InitializeError("parent of the current EOVariable is not set.")
+        return self._parent
+
+    @parent.setter
+    def parent(self, value: "EOGroup"):
+        if not isinstance(value, EOGroup):
+            raise ValueError(
+                f"parent of EOVariable instance must be EOGroup instance, but your have send {type(value)} instance.",
+            )
+        self._parent = value
+
+    # @abstractmethod
+    # def __array_ufunc__(self, ufunc, method, *inputs, **kwargs):
+    #     """Must be compliant to the NEP 13.
+    #     https://numpy.org/neps/nep-0013-ufunc-overrides.html
+    #     """
+
+    # @abstractmethod
+    # def __array_function__(self, func, types, args, kwargs):
+    #     """Must be compliant to th NEP 18.
+    #     https://numpy.org/neps/nep-0018-array-function-protocol.html
+    #     """
+
+    # @abstractmethod
+    # def __dask_graph__(self) -> Union[Mapping, None]:
+    #     """ """
+
+    # @abstractmethod
+    # def __dask_keys__(self) -> list:
+    #     """ """
+
+    # @abstractmethod
+    # def __dask_layers__(self) -> tuple:
+    #     """ """
+
+    # @abstractmethod
+    # def __dask_postcompute__(self) -> tuple[Callable, tuple]:
+    #     """ """
+
+    # @abstractmethod
+    # def __dask_postpersist__(self) -> tuple[Callable, tuple]:
+    #     """ """
+
+    # @abstractmethod
+    # def __dask_tokenize__(self) -> Hashable:
+    #     """Dask deterministic Hashing:
+    #     https://docs.dask.org/en/stable/custom-collections.html#deterministic-hashing"""
+
+    # @staticmethod
+    # @abstractmethod
+    # def __dask_scheduler__(dsk, keys, **kwargs):
+    #     """ """
+
+    # @property
+    # @abstractmethod
+    # def chunks(self) -> Optional[tuple[tuple[int, ...], ...]]:
+    #     """Accessor to the chunks"""
+
+    # @abstractmethod
+    # def rechunk(
+    #     self,
+    #     chunks_shape: Union[
+    #         Mapping[Any, Union[None, int, tuple[int, ...]]],
+    #         int,
+    #         tuple[int, ...],
+    #         tuple[tuple[int, ...], ...],
+    #     ],
+    # ) -> None:
+    #     """Change chunk shape / size"""
+
+    # @abstractmethod
+    # def map_chunk(self, func):
+    #     """map function on each chunk"""
 
 
-class EOGroup(EOProperties, MutableMapping[EOVariable]):
+class EOGroup(EOProperties, MutableMapping[str, EOVariable]):
     """Earth Observation Group definition
     Compliant to the Common data model and
     Climate and forecast conventions.
     """
 
-    _variables: MutableMapping[EOVariable]
+    __slots__ = (
+        "_name",
+        "_variables",
+        "_groups",
+        "_attrs",
+        "_dims",
+        "_coords",
+        "_parent",
+    )
 
-    @property
-    @abstractmethod
-    def groups(self) -> MutableMapping["EOGroup"]:
-        """Earth Obsetvation Sub Groups"""
+    def __types__(self) -> None:
+        self._name: str
+        self._variables: MutableMapping[str, EOVariable]
+        self._groups: MutableMapping[str, "EOGroup"]
+        self._attrs: MutableMapping[str, Any]
+        self._dims: tuple[str]
+        self._coords: Optional["EOGroup"] = None
+        self._parent: Optional[Union["EOGroup", "EOProduct"]] = None
 
-    def search(self, path, already_done: List["EOGroup"] = None):
-        """
-        Climate and forecast convention search mechanisme for group
-        https://cfconventions.org/Data/cf-conventions/cf-conventions-1.9/cf-conventions.html#_scope
-        """
-        already_done = already_done or []
-        already_done.append(self)
+    def __init__(
+        self,
+        name: str,
+        *args: EOVariable,
+        variables: Optional[Iterable[EOVariable]] = None,
+        groups: Optional[Iterable["EOGroup"]] = None,
+        coords: Optional["EOGroup"] = None,
+        attrs: Optional[MutableMapping[str, Any]] = None,
+        dims: Optional[tuple[str]] = None,
+    ) -> None:
+        self.__types__()
+        self._name = name
+        variables = [*args, *(variables or [])]
+        self._variables = {}
+        for variable in variables:
+            self[variable.name] = variable
 
-        root, _, subpaths = path.partition("/")
-        if not root:  # absolute path
-            parent = self.parent
-            while parent.parent is not None:
-                parent = parent.parent
-            return parent.search(subpaths)
-        elif root and subpaths:  # relative path
-            current_item = self.groups.get(root)
-            if current_item:
-                return current_item.search(subpaths)
-            return current_item
-        else:  # proximity
-            current_item = self.get(root)
-            if current_item:
-                return current_item
-            for group in self.parent.groups.values():
-                if group not in already_done:
-                    find = group.search(root, already_done=already_done)
-                    if find:
-                        return find
-        return
+        self._groups = {group.name: group for group in (groups or [])}
+        if coords:
+            self._coords = coords
+            self._coords.parent = self
+
+        self._attrs = {}
+        self._attrs.update(attrs or {})
+
+        self._dims = tuple(i for i in (dims or []))
 
     def __getitem__(self, key: str) -> EOVariable:
         return self._variables.__getitem__(key)
 
     def __setitem__(self, key: str, value: EOVariable) -> None:
         self._variables.__setitem__(key, value)
+        value.parent = self
 
     def __delitem__(self, key: str) -> None:
         self._variables.__delitem__[key]
@@ -195,9 +273,57 @@ class EOGroup(EOProperties, MutableMapping[EOVariable]):
     def __iter__(self) -> Iterator[EOVariable]:
         return self._variables.__iter__()
 
+    def __str__(self):
+        return self.__repr__()
+
+    def __repr__(self) -> str:
+        parent_part = f"(parent={self.parent.name or type(self.parent)})" if self._parent else ""
+        return f"[EOGroup]{parent_part}(dims={self.dims}) {self.name}"
+
+    @property
+    def attrs(self):
+        return self._attrs
+
+    @property
+    def coords(self) -> "EOGroup":
+        return self._coords
+
+    @property
+    def dims(self) -> tuple[str]:
+        return self._dims
+
+    @property
+    def groups(self) -> MutableMapping[str, "EOGroup"]:
+        return self._groups
+
+    @property
+    def name(self) -> Optional[str]:
+        return self._name
+
+    @property
+    def parent(self):
+        if self._parent is None:
+            raise exceptions.InitializeError("parent of the current EOGroup is not set.")
+        return self._parent
+
+    @parent.setter
+    def parent(self, value: Union["EOGroup", "EOProduct"]):
+        if not isinstance(value, (EOGroup, EOProduct)):
+            raise ValueError("parent of EOGroup instance must be EOGroup or EOProduct instance")
+        self._parent = value
+
+    def keys(self) -> KeysView["EOGroup"]:
+        return self._variables.keys()
+
+    def values(self) -> KeysView["EOGroup"]:
+        return self._variables.values()
+
 
 class MetaData(ABC):
-    path: Optional[str]
+    @property
+    @abstractmethod
+    def path(self):
+        """ """
 
     @property
     @abstractmethod
@@ -205,31 +331,51 @@ class MetaData(ABC):
         """ """
 
 
-class EOProduct(EOProperties, MutableMapping[EOGroup]):
+class EOProduct(EOProperties, MutableMapping[str, EOGroup]):
     """Earth Observation Top Group definition
     Compliant to the Common data model.
     """
 
-    _groups: MutableMapping[EOGroup]
+    __slots__ = ("_name", "_groups", "_coords", "_attrs", "_metadata")
 
-    @property
-    def name(self) -> None:
-        return None
+    def __types__(self):
+        self._name: str
+        self._groups: MutableMapping[str, EOGroup]
+        self._coords: EOGroup
+        self._attrs: MutableMapping[str, Any]
+        self._metadata: Optional[MetaData] = None
 
-    @property
-    def parent(self) -> None:
-        return None
+    def __init__(
+        self,
+        name: str,
+        coords: Optional[EOGroup],
+        *args: EOGroup,
+        groups: Optional[Iterable[EOGroup]] = None,
+        attrs: Optional[MutableMapping[str, Any]] = None,
+        metadata: Optional[MetaData] = None,
+    ) -> None:
+        self.__types__()
+        self._name = name
 
-    @property
-    @abstractmethod
-    def metadata(self) -> MetaData:
-        """ """
+        self._coords = coords
+        self._coords.parent = self
 
-    def __getitem__(self, key: str) -> EOVariable:
+        groups = [*args, *(groups or [])]
+        self._groups = {}
+        for group in groups:
+            self[group.name] = group
+
+        self._attrs = {}
+        self._attrs.update(attrs or {})
+
+        self._metadata = metadata
+
+    def __getitem__(self, key: str) -> EOGroup:
         return self._groups.__getitem__(key)
 
-    def __setitem__(self, key: str, value: EOVariable) -> None:
+    def __setitem__(self, key: str, value: EOGroup) -> None:
         self._groups.__setitem__(key, value)
+        value.parent = self
 
     def __delitem__(self, key: str) -> None:
         self._groups.__delitem__[key]
@@ -237,5 +383,41 @@ class EOProduct(EOProperties, MutableMapping[EOGroup]):
     def __len__(self) -> int:
         return self._groups.__len__()
 
-    def __iter__(self) -> Iterator[EOVariable]:
+    def __iter__(self) -> Iterator[EOGroup]:
         return self._groups.__iter__()
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __repr__(self) -> str:
+        return f"[EOProduct](metadata={self.metadata}) {hex(id(self))}"
+
+    @property
+    def attrs(self) -> MutableMapping[str, Any]:
+        return self._attrs
+
+    @property
+    def coords(self) -> EOGroup:
+        return self._coords
+
+    @property
+    def dims(self) -> tuple[str]:
+        return tuple()
+
+    @property
+    def metadata(self) -> Optional[MetaData]:
+        return self._metadata
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def parent(self) -> None:
+        return None
+
+    def keys(self) -> KeysView["EOProduct"]:
+        return self._groups.keys()
+
+    def values(self) -> KeysView["EOProduct"]:
+        return self._groups.values()
--- eopf/product/store.py
+++ eopf/product/store.py
@@ -2,12 +2,16 @@
 Define Store Objects for EOProduct.
 """
 
+import os
+import pathlib
 from abc import ABC, abstractclassmethod, abstractmethod
+from itertools import groupby
 from typing import Type
 
+import xarray
 from fsspec.spec import AbstractFileSystem
 
-from .core import EOProduct
+from .core import EOGroup, EOProduct, EOVariable
 
 
 class EOProductStore(ABC):
@@ -47,3 +51,44 @@ class EOProductStore(ABC):
     @abstractmethod
     def __exit__(self, *args):
         """ """
+
+
+def find_files_paths(abs_path, extension):
+    yield from (
+        pathlib.Path(os.path.join(abs_path, filename))
+        for filename in os.listdir(abs_path)
+        if os.path.splitext(filename)[-1] == extension
+    )
+
+
+def convert_nc_to_product(path, attr_file=None):
+    def group_order(file_path: str):
+        return file_path.name.split("_")[-1].replace(".nc", "")
+
+    attrs = None
+    product_name = pathlib.Path(path)
+    product_name = product_name.name.replace(product_name.suffix, "")
+
+    files = list(find_files_paths(path, ".nc"))
+    grouped_files = groupby(sorted(files, key=group_order), group_order)
+    groups = []
+    coords = None
+    for group_name, files in grouped_files:
+        dts = (
+            (xarray.open_dataset(file, decode_times=False, mask_and_scale=False), file.name.replace(file.suffix, ""))
+            for file in files
+        )
+        current_group = EOGroup(group_name)
+        for dt, name in dts:
+            variables = [EOVariable(value) for value in dt.values()]
+            if len(variables) == 1:
+                current_group[name] = variables[0]
+            else:
+                current_group.groups[name] = EOGroup(name, *variables, attrs=dt.attrs, dims=dt.dims)
+        if group_name == "coordinates":
+            coords = current_group
+        else:
+            groups.append(current_group)
+    attrs = xarray.open_dataset(attr_file, decode_times=False, mask_and_scale=False).attrs if attr_file else None
+    product = EOProduct(product_name, coords, *groups, attrs=attrs)
+    return product
