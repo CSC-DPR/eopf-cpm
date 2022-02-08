f8e085a2bb3e1932a2391ec251e605485edd430d
Lidia <lbaciu@c-s.ro>
2022-02-08 13:28:16 +0200

    feature: tests for xml persistance slstr and olci

--- tests/test_xml_functions.py
+++ tests/test_xml_functions.py
@@ -1,13 +1,23 @@
+import glob
 import os
 
 import pytest
 
+from eopf.product.constants import (
+    CF_MAP_OLCI_L1,
+    CF_MAP_SLSTR_L1,
+    EOP_MAP_OLCI_L1,
+    EOP_MAP_SLSTR_L1,
+    NAMESPACES_OLCI_L1,
+    NAMESPACES_SLSTR_L1,
+)
 from eopf.product.conveniences import (
     apply_xpath,
     etree_to_dict,
     parse_xml,
     translate_structure,
 )
+from eopf.product.convert import OLCIL1EOPConverter, SLSTRL1EOPConverter
 
 
 @pytest.mark.unit
@@ -108,3 +118,51 @@ def test_etree_to_dict():
             },
         },
     }
+
+
+@pytest.mark.unit
+def test_xml_persistance_olci():
+    olci_path = glob.glob("data/S3A_OL_1*.SEN3")[0]
+    olci_eop = OLCIL1EOPConverter(olci_path)
+    olci_eop.read()
+
+    # Legacy attributes
+    tree = parse_xml(olci_path, "xfdumanifest.xml")
+    root = tree.getroot()
+    xfdu_dict = etree_to_dict(root[1])
+    cf_dict = {attr: apply_xpath(tree, CF_MAP_OLCI_L1[attr], NAMESPACES_OLCI_L1) for attr in CF_MAP_OLCI_L1}
+    eop_dict = {attr: translate_structure(EOP_MAP_OLCI_L1[attr], tree, NAMESPACES_OLCI_L1) for attr in EOP_MAP_OLCI_L1}
+
+    # EOProduct attributes
+    cf_eop_dict = olci_eop.eop.attributes.attrs["CF"]
+    om_eop_dict = olci_eop.eop.attributes.attrs["OM-EOP"]
+    xfdu_eop_dict = olci_eop.eop.attributes.attrs["XFDU"]
+
+    assert xfdu_dict == xfdu_eop_dict
+    assert cf_dict == cf_eop_dict
+    assert eop_dict == om_eop_dict
+
+
+@pytest.mark.unit
+def test_xml_persistance_slstr():
+    slstr_path = glob.glob("data/S3A_SL_1_RBT*.SEN3")[0]
+    slstr_eop = SLSTRL1EOPConverter(slstr_path)
+    slstr_eop.read()
+
+    # Legacy attributes
+    tree = parse_xml(slstr_path, "xfdumanifest.xml")
+    root = tree.getroot()
+    xfdu_dict = etree_to_dict(root[1])
+    cf_dict = {attr: apply_xpath(tree, CF_MAP_SLSTR_L1[attr], NAMESPACES_SLSTR_L1) for attr in CF_MAP_SLSTR_L1}
+    eop_dict = {
+        attr: translate_structure(EOP_MAP_SLSTR_L1[attr], tree, NAMESPACES_SLSTR_L1) for attr in EOP_MAP_SLSTR_L1
+    }  # noqa
+
+    # EOProduct attributes
+    cf_eop_dict = slstr_eop.eop.attributes.attrs["CF"]
+    om_eop_dict = slstr_eop.eop.attributes.attrs["OM-EOP"]
+    xfdu_eop_dict = slstr_eop.eop.attributes.attrs["XFDU"]
+
+    assert xfdu_dict == xfdu_eop_dict
+    assert cf_dict == cf_eop_dict
+    assert eop_dict == om_eop_dict
