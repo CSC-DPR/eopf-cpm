0d7f2c21eab0f45a3509ffaafa0f0190ee185bac
Guillaume Beauchamp <guillaume.beauchamp@csgroup.eu>
2022-02-21 17:12:35 +0100

    feat : add a mapping factory

--- eopf/product/store/mapping_factory.py
+++ eopf/product/store/mapping_factory.py
@@ -1,28 +1,22 @@
-from typing import Optional
-
-from eopf.product.store import EOProductStore
-from eopf.product.store.zarr import EOZarrStore
+import json
+from typing import Any
 
 
-class StoreMappingFactory:
+class MappingFactory:
     def __init__(self, default_mappings=True):
-        self.item_format_mappings = dict()
-        self.store_types = set()
+        self.mapping_set = set()
         if default_mappings:
-            self.register_mapping(EOZarrStore)
-            # self.register_mapping(EONetcdfStore, "netcdf")
+            self.register_mapping("S3_OL_1_EFR_mapping.json")
 
-    def register_mapping(self, store_class: type, *args: str) -> None:
-        self.store_types.add(store_class)
-        for mapping in args:
-            self.item_format_mappings[mapping] = store_class
+    def register_mapping(self, store_class: str) -> None:
+        self.mapping_set.add(store_class)
 
-    def get_mapping(self, file_path: str, item_format: Optional[str] = None, *args, **kwargs) -> EOProductStore:
-        if item_format is not None:
-            if item_format in self.item_format_mappings:
-                return self.item_format_mappings[item_format](file_path, *args, **kwargs)
-            raise KeyError("No registered store with format : " + item_format)
-        for store_type in self.store_types:
-            if store_type.guess_can_read(file_path):
-                return store_type(file_path, *args, **kwargs)
+    def get_mapping(self, file_path: str) -> dict[str, Any]:
+        for json_mapping_file in self.mapping_set:
+            json_mapping_data = json.load(json_mapping_file)
+            if self.guess_can_read(json_mapping_data, file_path):
+                return json_mapping_data
         raise KeyError("No registered store compatible with : " + file_path)
+
+    def guess_can_read(self, json_mapping_data: dict[str, Any], file_path: str):
+        return True
