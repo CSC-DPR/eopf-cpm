dd95587a188c00ed59e99b3fc2f9f9d5c4b0ad48
Clément Dubos <clement.dubos.ext@csgroup.eu>
2022-03-04 16:22:25 +0100

    fix: html tree display

--- eopf/product/formatting.py
+++ eopf/product/formatting.py
@@ -1,11 +1,14 @@
+import json
 import os
 from pathlib import Path
 from typing import Any
 
 from jinja2 import Environment, FileSystemLoader
 
+from eopf.product.utils import conv
 
-def renderer(template_name: str, **parameters: Any) -> str:
+
+def renderer(template_name: str, attrs: dict[str, Any] = {}, **parameters: Any) -> str:
     """Render a eopf template from the given name
 
     Parameters
@@ -19,9 +22,14 @@ def renderer(template_name: str, **parameters: Any) -> str:
     -------
     str
     """
+
+    def human_readable_attrs(value):
+        return json.dumps(conv(value), indent=4)
+
     dir_path = Path(__file__).resolve().parent
     file_loader = FileSystemLoader(os.path.join(dir_path, "templates"))
     env = Environment(loader=file_loader, autoescape=True)
+    env.filters["human_readable_attrs"] = human_readable_attrs
     template = env.get_template(template_name)
     html = template.render({**parameters, "is_top": True})
     return html
