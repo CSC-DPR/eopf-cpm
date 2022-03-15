43ba7cff48a0b19c551d2b4e4e7dca46d94cdd19
Alex Becheru <abecheru@c-s.ro>
2022-03-15 14:21:58 +0200

    refactor: removed comments and moved functions to respect the harmozied structure

--- eopf/product/store/netcdf.py
+++ eopf/product/store/netcdf.py
@@ -17,7 +17,13 @@ if TYPE_CHECKING:  # pragma: no cover
 class EONetCDFStore(EOProductStore):
     """
     Store representation to access NetCDF format of the given URL
+
     Parameters
+    ----------
+    url: str
+        path url or the target store
+
+    Attributes
     ----------
     url: str
         path url or the target store
@@ -28,13 +34,6 @@ class EONetCDFStore(EOProductStore):
 
     # docstr-coverage: inherited
     def __init__(self, url: str) -> None:
-        """Instantiate based on the url(file path) of the manifest netCDF4 file
-
-        Parameters
-        ----------
-        url: str,
-            path to netCDF4 file
-        """
         url = os.path.expanduser(url)
         super().__init__(url)
         self._root: Optional[Dataset] = None
@@ -147,20 +146,7 @@ class EONetCDFStore(EOProductStore):
         return it.chain(iter(current_node.groups), iter(current_node.variables))
 
     def __getitem__(self, key: str) -> "EOObject":
-        """Getter for netCDF4 variable or group
 
-        Parameters
-        ----------
-        key: str
-            Name of group or variable
-        Raises
-        ------
-        StoreNotOpenError
-            If the store is closed
-        Returns
-        ----------
-        EOObject
-        """
         if self._root is None:
             raise StoreNotOpenError("Store must be open before access to it")
 
@@ -174,20 +160,20 @@ class EONetCDFStore(EOProductStore):
             return EOGroup(attrs=decode_attrs(obj.__dict__))
         return EOVariable(data=obj, attrs=decode_attrs(obj.__dict__), dims=obj.dimensions)
 
-    def __setitem__(self, key: str, value: "EOObject") -> None:
-        """Setter for netCDF4 variable or group
+    def __iter__(self) -> Iterator[str]:
 
-        Parameters
-        ----------
-        key: str
-            Name of group or variable
-        value: EOObject
-            Value of EOObject
-        Raises
-        ------
-        StoreNotOpenError
-            If the store is closed
-        """
+        if self._root is None:
+            raise StoreNotOpenError("Store must be open before access to it")
+        return it.chain(iter(self._root.groups), iter(self._root.variables))
+
+    def __len__(self) -> int:
+
+        if self._root is None:
+            raise StoreNotOpenError("Store must be open before access to it")
+        return len(self._root.groups) + len(self._root.variables)
+
+    def __setitem__(self, key: str, value: "EOObject") -> None:
+
         from eopf.product.core import EOGroup, EOVariable
 
         if self._root is None:
@@ -217,52 +203,14 @@ class EONetCDFStore(EOProductStore):
             raise TypeError("Only EOGroup and EOVariable can be set")
         self.write_attrs(key, value.attrs)
 
-    def __len__(self) -> int:
-        """Used to return total length of an EOOBject
+    # docstr-coverage: inherited
+    def close(self) -> None:
 
-        Returns
-        ----------
-        Int
-        Raises
-        ------
-        StoreNotOpenError
-            If the store is closed
-        """
         if self._root is None:
             raise StoreNotOpenError("Store must be open before access to it")
-        return len(self._root.groups) + len(self._root.variables)
-
-    def __iter__(self) -> Iterator[str]:
-        """Iterator over each variable based on its group
-
-        Returns
-        ----------
-        An iterator over groups and variables
-        Raises
-        ------
-        StoreNotOpenError
-            If the store is closed
-        """
-        if self._root is None:
-            raise StoreNotOpenError("Store must be open before access to it")
-        return it.chain(iter(self._root.groups), iter(self._root.variables))
-
-    def _select_node(self, key: str) -> Union[Dataset, Group, Variable]:
-        """Getter for netCDF4 Dataset Variable
-
-        Returns
-        Union of Dataset, group, variable
-        ----------
-        Raises
-        ------
-        StoreNotOpenError
-            If the store is closed
-        """
-        if self._root is None:
-            raise StoreNotOpenError("Store must be open before access to it")
-        if key in ["/", ""]:
-            return self._root
-        return self._root[key]
+        super().close()
+        self._root.close()
+        self._root = None
 
     # docstr-coverage: inherited
     @staticmethod
@@ -278,3 +226,87 @@ class EONetCDFStore(EOProductStore):
         Boolean
         """
         return pathlib.Path(file_path).suffix in [".nc"]
+
+    # docstr-coverage: inherited
+    def is_group(self, path: str) -> bool:
+
+        if self._root is None:
+            raise StoreNotOpenError("Store must be open before access to it")
+        current_node = self._select_node(path)
+        return isinstance(current_node, (Group, Dataset))
+
+    # docstr-coverage: inherited
+    def is_variable(self, path: str) -> bool:
+
+        if self._root is None:
+            raise StoreNotOpenError("Store must be open before access to it")
+        current_node = self._select_node(path)
+        return isinstance(current_node, Variable)
+
+    # docstr-coverage: inherited
+    def iter(self, path: str) -> Iterator[str]:
+
+        if self._root is None:
+            raise StoreNotOpenError("Store must be open before access to it")
+        current_node = self._select_node(path)
+        return it.chain(iter(current_node.groups), iter(current_node.variables))
+
+    # docstr-coverage: inherited
+    def open(self, mode: str = "r", **kwargs: Any) -> None:
+
+        super().open()
+        # Overwrite compression / scale parameters if given by user
+        if "zlib" in kwargs:
+            self.zlib = bool(kwargs.get("zlib"))
+            kwargs.pop("zlib")
+        if "complevel" in kwargs:
+            self.complevel = int(str(kwargs.get("complevel")))
+            kwargs.pop("complevel")
+        if "shuffle" in kwargs:
+            self.shuffle = bool(kwargs.get("shuffle"))
+            kwargs.pop("shuffle")
+        if "scale" in kwargs:
+            self.scale = bool(kwargs.get("scale"))
+            kwargs.pop("scale")
+        self._root = Dataset(self.url, mode, **kwargs)
+
+    def write_attrs(self, group_path: str, attrs: MutableMapping[str, Any] = {}) -> None:
+        """
+        This method is used to update attributes in the store
+
+        Raises
+        ------
+        StoreNotOpenError
+            If the store is closed
+        """
+        if self._root is None:
+            raise StoreNotOpenError("Store must be open before access to it")
+        current_node = self._select_node(group_path)
+        from json import dumps
+
+        # convert attributes to python data types
+        # if not a number convert to json dict
+        # since netCDF4 does not allow dictionary imbrication
+        if group_path == "/" or group_path == "":
+            attrs = {attr: dumps(conv(value)) for attr, value in attrs.items() if attr not in self.RESTRICTED_ATTR_KEY}
+        else:
+            attrs = {attr: conv(value) for attr, value in attrs.items() if attr not in self.RESTRICTED_ATTR_KEY}
+
+        current_node.setncatts(attrs)
+
+    def _select_node(self, key: str) -> Union[Dataset, Group, Variable]:
+        """Retrieve and return the netcdf4 object corresponding to the node at the given path
+
+        Returns
+        Union of Dataset, Group, Variable
+        ----------
+        Raises
+        ------
+        StoreNotOpenError
+            If the store is closed
+        """
+        if self._root is None:
+            raise StoreNotOpenError("Store must be open before access to it")
+        if key in ["/", ""]:
+            return self._root
+        return self._root[key]
