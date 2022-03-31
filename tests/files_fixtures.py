6563b6bb8036e1a5b0512dfd86aa9c91c68b3d04
Clément Dubos <clement.dubos.ext@csgroup.eu>
2022-03-31 10:26:07 +0200

    fix: work with a TEST_DATA_FOLDER

--- tests/files_fixtures.py
+++ tests/files_fixtures.py
@@ -6,11 +6,12 @@ import pytest
 from .utils import PARENT_DATA_PATH
 
 TEST_DATA_FOLDER = os.environ.get("TEST_DATA_FOLDER", os.path.join(PARENT_DATA_PATH, "data"))
-print(os.path.exists(TEST_DATA_FOLDER))
 
 
 @pytest.fixture
 def S3_OLCI_L1_EFR():
     file_name = "S3*_OL_1_E*R*.zip"
+    print(os.path.exists(TEST_DATA_FOLDER))
+
     glob_path = os.path.join(TEST_DATA_FOLDER, file_name)
     return f"zip::file://{glob.glob(glob_path)[0]}"
