cbd5db6bbdb125f6f0d8ede37edb9641ff127302
Alex Becheru <abecheru@c-s.ro>
2022-05-24 00:53:53 +0300

    fix: style hooks

--- tests/test_log.py
+++ tests/test_log.py
@@ -1,22 +1,18 @@
 from logging import Logger
 from pathlib import Path
-from tempfile import tempdir
+
 import pytest
-from eopf.logging import EOLogFactory
 
 from eopf.exceptions import (
-    DaskProfilerError,
     LoggingConfigurationAlreadyExists,
     LoggingConfigurationDirDoesNotExist,
     LoggingConfigurationFileIsNotValid,
     LoggingConfigurationFileTypeNotSupported,
     LoggingConfigurationNotRegistered,
-    SingleThreadProfilerError,
 )
-from eopf.exceptions.warnings import (
-    DaskProfilerHtmlDisplayNotWorking,
-    NoLoggingConfigurationFile,
-)
+from eopf.exceptions.warnings import NoLoggingConfigurationFile
+from eopf.logging import EOLogFactory
+
 
 @pytest.mark.unit
 def test_correct_init():
@@ -43,7 +39,7 @@ def test_correct_return_yaml():
 
 @pytest.mark.unit
 def test_unregistered_configuration():
-    """Test that when trying to get log without a registered configuration it raises LoggingConfigurationNotRegistered"""
+    """Test when trying to get log without a registered configuration it raises LoggingConfigurationNotRegistered"""
     test_factory = EOLogFactory()
     with pytest.raises(LoggingConfigurationNotRegistered):
         _ = test_factory.get_log(cfg_name="default2")
@@ -85,9 +81,6 @@ def test_register_cfg_with_incorrect_file_path():
 def test_register_cfg_with_incorrect_file_extension():
     """Test that when setting the cfg dir with a non-existent dir it raises LoggingConfigurationDirDoesNotExist"""
 
-    #import tempfile
-    #tmp_cfg_file = tempfile.mktemp(prefix="conf", suffix=".yam")
-    #tmp_cfg_file = tmpdir.mkdir("data") / "a.cof"
     test_factory = EOLogFactory()
     test_file_path = Path(__file__).parent / "data" / "log_conf.yam"
     with pytest.raises(LoggingConfigurationFileTypeNotSupported):
@@ -95,7 +88,6 @@ def test_register_cfg_with_incorrect_file_extension():
 
 
 @pytest.mark.unit
-
 def test_register_cfg_dir_with_no_log_configurations():
     """Test that when setting the cfg dir with a non-existent dir it raises NoLoggingConfigurationFile"""
 
@@ -103,7 +95,7 @@ def test_register_cfg_dir_with_no_log_configurations():
     test_dir_path = Path(__file__).parent / "data" / "empty_dir"
     with pytest.raises(NoLoggingConfigurationFile):
         test_factory.set_cfg_dir(test_dir_path)
-        
+
 
 @pytest.mark.unit
 def test_get_log_with_non_valid_configuration_file():
@@ -114,20 +106,3 @@ def test_get_log_with_non_valid_configuration_file():
     test_factory.register_cfg("invalid", cfg_path=test_file_path)
     with pytest.raises(LoggingConfigurationFileIsNotValid):
         _ = test_factory.get_log("invalid")
-    
-    
-    
-    
-    
-    
-    
-
-
-    
-    
-    
-
-
-    
-    
-
