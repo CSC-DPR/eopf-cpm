e2dc387c3d30247c748cb22cd8108880fa63c43c
Alex Becheru <abecheru@c-s.ro>
2022-05-24 11:24:20 +0300

    tests: added tests for profile decorators

--- tests/test_log.py
+++ tests/test_log.py
@@ -1,17 +1,22 @@
 from logging import Logger
 from pathlib import Path
+from pstats import Stats
 
+import dask.array as da
 import pytest
 
 from eopf.exceptions import (
+    DaskProfilerError,
     LoggingConfigurationAlreadyExists,
     LoggingConfigurationDirDoesNotExist,
     LoggingConfigurationFileIsNotValid,
     LoggingConfigurationFileTypeNotSupported,
     LoggingConfigurationNotRegistered,
+    SingleThreadProfilerError,
 )
 from eopf.exceptions.warnings import NoLoggingConfigurationFile
-from eopf.logging import EOLogFactory
+from eopf.logging import EOLogFactory, dask_profiler
+from eopf.logging.log import single_thread_profiler
 
 
 @pytest.mark.unit
@@ -106,3 +111,83 @@ def test_get_log_with_non_valid_configuration_file():
     test_factory.register_cfg("invalid", cfg_path=test_file_path)
     with pytest.raises(LoggingConfigurationFileIsNotValid):
         _ = test_factory.get_log("invalid")
+
+
+@pytest.mark.unit
+def test_dask_profiler_nominal():
+    expected_return_value = 100
+    expected_parameter_value = 2
+
+    @dask_profiler(
+        n_workers=3,
+        threads_per_worker=1,
+        report_name="data/test-dask-report.html",
+        display_report=False,
+    )
+    def just_a_simple_dask_func(a_parameter: int):
+        # test parameters are passed correctly
+        assert isinstance(a_parameter, int) and a_parameter == expected_parameter_value
+
+        # some dask computation
+        x = da.arange(10, chunks=10)
+        x.sum().compute()
+
+        return expected_return_value
+
+    # test that the decorated function returns the expected value
+    assert just_a_simple_dask_func(expected_parameter_value) == expected_return_value
+
+
+@pytest.mark.unit
+def test_dask_profiler_raises_exception():
+    """Test that DaskProfilerError is raised when the decorated function raises an exception"""
+
+    @dask_profiler(
+        n_workers=3,
+        threads_per_worker=1,
+        report_name="data/test-dask-report.html",
+        display_report=False,
+    )
+    def just_a_simple_dask_func():
+        raise Exception("Just an exception")
+
+    # test that an Exception is raised
+    with pytest.raises(DaskProfilerError):
+        _ = just_a_simple_dask_func()
+
+
+@pytest.mark.unit
+def test_single_threaded_profiler_nominal():
+    """Test nominal functioning of the single_thread_profiler"""
+    expected_return_type = Stats
+    expected_parameter_value = 2
+
+    @single_thread_profiler(
+        report_name="data/single-thread-report",
+    )
+    def just_a_simple_dask_func(a_parameter: int):
+        # test parameters are passed correctly
+        assert isinstance(a_parameter, int) and a_parameter == expected_parameter_value
+
+        # some dask computation
+        x = da.arange(10, chunks=10)
+        x.sum().compute()
+
+        return 100
+
+    assert isinstance(just_a_simple_dask_func(expected_parameter_value), expected_return_type)
+
+
+@pytest.mark.unit
+def test_single_thread_profiler_raises_exception():
+    """Test that SingleThreadProfilerError is raised when the decorated function raises an exception"""
+
+    @single_thread_profiler(
+        report_name="data/single-thread-report",
+    )
+    def just_a_simple_dask_func():
+        raise Exception("Just an exception")
+
+    # test that an Exception is raised
+    with pytest.raises(SingleThreadProfilerError):
+        _ = just_a_simple_dask_func()
