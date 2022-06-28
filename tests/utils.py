e6888a33ff2aebadf0421493bb3fd2a86050e8aa
Clément Dubos <clement.dubos.ext@csgroup.eu>
2022-06-28 09:51:10 +0200

    feat: TEST_ONLY_ONE_PRODUCT as boolean

--- tests/utils.py
+++ tests/utils.py
@@ -169,7 +169,7 @@ PARENT_DATA_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), ".."
 TEST_DATA_PATH = os.environ.get("TEST_DATA_FOLDER", os.path.join(PARENT_DATA_PATH, "data"))
 EMBEDED_TEST_DATA_PATH = os.path.join(PARENT_DATA_PATH, "tests", "data")
 MAPPING_PATH = os.path.join(PARENT_DATA_PATH, "eopf", "product", "store", "mapping")
-TEST_ONLY_ONE_PRODUCT = os.environ.get("TEST_ONLY_ONE_PRODUCT")
+TEST_ONLY_ONE_PRODUCT = os.environ.get("TEST_ONLY_ONE_PRODUCT") in [True, "True", "true", 1, "1"]
 
 
 def glob_fixture(
