fd1653df81f64d51b910208906c872c090e0bf1a
Adrian Tabacioiu <Adi.Tabacioiu@c-s.ro>
2022-06-29 16:23:29 +0300

    fix: for integration tests

--- tests/utils.py
+++ tests/utils.py
@@ -122,7 +122,7 @@ def assert_eovariable_equal(variable1, variable2):
     elif source_data.dtype == np.dtype("S1") or target_data.dtype == np.dtype("S1"):
         assert np.array_equal(source_data, target_data)
     else:
-        assert np.array_equal(source_data, target_data, equal_nan=True)
+        assert np.array_equal(source_data, target_data, equal_nan=False)
 
 
 def assert_is_subeocontainer(container1, container2):
