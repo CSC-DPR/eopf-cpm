41a244635d844ff76109143e2b39c9923b3640c3
Guillaume Beauchamp <guillaume.beauchamp@csgroup.eu>
2022-07-19 14:45:24 +0200

    test: fix test mapping

--- eopf/product/core/eo_variable.py
+++ eopf/product/core/eo_variable.py
@@ -66,7 +66,7 @@ class EOVariable(EOObject, EOVariableOperatorsMixin["EOVariable"]):
             try:
                 lazy_data = da.asarray(data)
             except NotImplementedError:
-                # Some types of data don't support a method used by dask 
+                # Some types of data don't support a method used by dask
                 # when it ask the data size in order to choose the optimal chunk size.
                 lazy_data = da.asarray(data, chunks=2000)
             if not hasattr(data, "dtype"):
